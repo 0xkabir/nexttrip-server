ca9bf009def95cdd888e402e9567308bf9d5c15d
Shamimayesmin <shamimayesmin144@gmail.com>
2023-04-14 13:06:12 +0600

    new add

--- index.js
+++ index.js
@@ -10,16 +10,21 @@ app.use(cors())
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ipnjwkc.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-app.get('/', (request, response) => {
-    response.send("NextTrip Server Online")
-})
+
 
 async function run() {
     try {
         const database = client.db('bookTheTour')
         const toursCollection = database.collection('tours')
+
+        app.get('/tours', async(request, response) => {
+            const query = {}
+            tours = await toursCollection.find(query).toArray()
+            response.send(tours);
+        })
     }
 
     finally{
@@ -29,6 +34,10 @@ async function run() {
 
 run().catch((error) => console.error(error));
 
+
+app.get('/', (request, response) => {
+    response.send("NextTrip Server Online")
+});
 app.listen(port, () => {
     console.log(`NextTrip sever online at port ${port}`)
-})
+});
